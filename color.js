92423a7c15e8f1804534786240f9b2ed41393480
Hamza Ali Rizvi <hamzaali151214@hotmail.com>
2019-12-09 23:46:24 +0530

    added strokeWeight() stuff to colors.js

--- color.js
+++ color.js
@@ -8,6 +8,7 @@ function setup() {
     for(let j=0;j<20;j++){
       for(let i=0;i<20;i++){
       fill(100,200,200,80)//decides inside color
+      strokeWeight(0.25*i)//pixel size of stroke
       stroke(255,59,129)//decides outline color
       arc(100+i*10,100+j*10,80,80,0,0.75*2*PI,PIE)
       }
