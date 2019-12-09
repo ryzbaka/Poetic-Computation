bc55dcae457ddf81f4c76638809968381f56dbf5
Hamza Ali Rizvi <hamzaali151214@hotmail.com>
2019-12-09 22:50:07 +0530

    basics

---
+++ sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+    createCanvas(400, 400);
+}
+
+function draw() {
+    background("white");
+    fill("pink");
+    arc(50, 50, 80, 80, 0, 0.75 * 2 * PI);
+    line(50, 50, 100, 100);
+}
